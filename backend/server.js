2ecdc781d8e0e65611b927746658e1683e203edc
s4nat <65476084+s4nat@users.noreply.github.com>
2023-09-05 23:37:07 +0800

    set up node package + express app

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Brown Munde Digital Wallet." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
