e5d957d0786907c3fd00541219ed0927fd4c89dc
Fauzaan <fauzaan.mm@gmail.com>
2023-09-08 00:30:47 +0800

    Differentiate production and development environments

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
-module.exports = {
-    HOST: process.env.HOST || "localhost",
-    USER: process.env.USER || "postgres",
+
+var configs = {};
+
+console.log(`\n\nYou are currently in ${process.env.ENVIRONMENT} environment.\n\n`)
+if (process.env.ENVIRONMENT == "dev") {
+  configs = {
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.LOCAL_PASSWORD,
+    DB: "wallet_tt",
+    dialect: "postgres",
+    sslmode: "disable"
+  };
+}
+
+else if (process.env.ENVIRONMENT == "prod") {
+  configs = {
+    HOST: process.env.POSTGRES_HOST,
+    USER: process.env.POSTGRES_USER,
     PASSWORD: process.env.POSTGRES_PASSWORD,
     DB: "verceldb",
     dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    sslmode: "require"
   };
+}
+
+
+module.exports = configs;
--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,7 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(`postgres://${dbConfig.USER}:${dbConfig.PASSWORD}@${dbConfig.HOST}:5432/${dbConfig.DB}?sslmode=require`)
+const sequelize = new Sequelize(`postgres://${dbConfig.USER}:${dbConfig.PASSWORD}@${dbConfig.HOST}:5432/${dbConfig.DB}?sslmode=${dbConfig.sslmode}`)
 
 const db = {};
 
@@ -11,6 +11,5 @@ db.sequelize = sequelize;
 
 db.transactions = require("./transaction.model.js")(sequelize, Sequelize);
 db.users = require("./user.model.js")(sequelize, Sequelize);
-db.contacts = require("./contact.model.js")(sequelize, Sequelize);
 
 module.exports = db;
