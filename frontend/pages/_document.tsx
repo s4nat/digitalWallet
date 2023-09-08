b24e1381675945470529a203dd7b7e0da179fc76
rmurarishetti <64725000+rmurarishetti@users.noreply.github.com>
2023-09-08 23:43:05 +0800

    feat: Pages Completed

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Navbar } from './components/Navbar'
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Navbar/>
       <Head />
       <body>
         <Main />
