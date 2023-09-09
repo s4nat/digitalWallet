5296c668821241eb2dcd0cf8fded56ff1d332eec
Fauzaan <fauzaan.mm@gmail.com>
2023-09-09 22:19:38 +0800

    Make deployment run

--- frontend/pages/components/Icon.tsx
+++
@@ -1,6 +0,0 @@
-import { BiSolidCookie } from "react-icons/bi";
-
-<div className="pl-20 flex font-sans text-[65px] font-medium text-[#635dff]">
-    <BiSolidCookie className="w-[100px] h-[70px] text-[#635dff]"/>
-    ByteWallet
-</div>
